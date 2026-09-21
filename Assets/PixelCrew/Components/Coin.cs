54bfb46e2099a41a9ea7b0caa4dc857a36867bc3
Sergey-81 <s_nizhegorodov@mail.ru>
2026-09-21 19:49:24 +0300

    Add coin pickup with wallet total

---
+++ Assets/PixelCrew/Components/Coin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class Coin : MonoBehaviour
+    {
+        [SerializeField] private int _value = 1;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var wallet = other.GetComponent<Wallet>();
+            if (wallet == null)
+                return;
+
+            wallet.Add(_value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/PixelCrew/Components/Wallet.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class Wallet : MonoBehaviour
+    {
+        public int Total { get; private set; }
+
+        public void Add(int amount)
+        {
+            Total += amount;
+            Debug.Log($"Монет собрано: {Total}");
+        }
+    }
+}
